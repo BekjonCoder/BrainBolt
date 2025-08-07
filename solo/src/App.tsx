06c7ee2264a816821b35f8eb28f9602e6ca59841
BekjonCoder <k75154256@gmail.com>
2025-08-07 21:39:57 +0500

    add

--- solo/src/App.tsx
+++ solo/src/App.tsx
@@ -2,17 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
 import QuizPage from "./pages/QuizPage";
-import LevelsPage from "./pages/LevelsPage"; // yangi sahifa
+import LevelsPage from "./pages/LevelsPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/levels/:category" element={<LevelsPage />} /> {/* fan tanlansa darajalar sahifasi ochiladi */}
-        <Route path="/quiz/:category/:level" element={<QuizPage />} /> {/* daraja tanlansa quiz sahifasi ochiladi */}
+        <Route path="/levels/:category" element={<LevelsPage />} />
+        <Route path="/quiz/:category/:level" element={<QuizPage />} />
         <Route path="/result" element={<Result />} />
-        {/* <Route path="*" element={<NotFound />} /> // optional */}
       </Routes>
     </BrowserRouter>
   );
